a0289d28a4d8e18b82f3fc691e60608bbea2357f
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-09 19:09:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    const output = array.find(element => (element.result === 'W'));
+    if (output === undefined) {
+        return output;
+    } else {
+        return output.year;
+    }
+}
